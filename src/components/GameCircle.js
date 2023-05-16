3059f25ad7e81a36fd0f3d1d92ee5569cae47ad4
Yasvanth Hanumantu <yasvanthhanumantu1@gmail.com>
2023-05-16 10:57:32 +0530

    Game Board

--- src/components/GameCircle.js
+++ src/components/GameCircle.js
@@ -4,9 +4,16 @@ const clickMsg = (id) =>{
   alert("You clicked "+ id);
 }
 
-export default function GameCircle({id,children}) {
+export default function GameCircle({id,color,children}) {
+  const style = {
+    backgroundColor : color,
+    width:100,
+    height:100,
+    borderRadius:'50%',
+    margin:10
+  }
   return (
-    <div onClick={() => clickMsg(id)}>
+    <div style={style} onClick={() => clickMsg(id)}>
         {children}
     </div>
   )
